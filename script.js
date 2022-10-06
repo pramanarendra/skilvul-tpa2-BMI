4cbce3aede7118e9bf0bc31c43f28217ee6482cd
pramanarendra <albertushandypramana@gmail.com>
2022-10-06 15:12:10 +0700

    inisialisasi git

---
+++ script.js
@@ -0,0 +1,44 @@
+// Get DOM elements
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
+let form = document.getElementById("form");
+
+
+// Input validation
+const validate = () => {
+    if(weight.value == "" || height.value == "" || parseInt(weight.value) < 0 || parseInt(height.value) < 0) {
+        alert("Input is not valid");
+        form.reset();
+    } else {
+        countBMI();
+    }
+}
+
+// BMI calculator
+const countBMI= () => {
+    let condition = "";
+    let BMI = (weight.value / (height.value / 100)**2).toFixed(1);
+
+    // BMI category
+    if (BMI < 18.5) {
+        condition = "underweight";
+    } else if (BMI <= 24.9) {
+        condition = "normal";
+    } else if (BMI <= 29.9) {
+        condition = "overweight";
+    } else if (BMI >= 30) {
+        condition = "obese";
+    }
+
+    // Show the result in HTML
+    document.getElementById("BMI").innerText = BMI;
+    document.getElementById("category").innerText = condition;
+    document.getElementById("result").style.visibility = "visible";
+
+    // Reset input
+    form.reset();
+}
+
+// Submit form event
+document.getElementById("submit").addEventListener("click", validate);
+
